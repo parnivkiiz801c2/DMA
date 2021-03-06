e1ea4378a1ef859a575e5e3ce48b2f9e6db438c8
parnivkiiz801c2 <kulikovrs@mer.ci.nsu.ru>
2021-03-06 16:22:36 +0700

    DMA_LastUpdate

--- First.s
+++ First.s
@@ -9,12 +9,11 @@ Stack_Top
 	AREA	RESET,	DATA,	READONLY
 	DCD		Stack_Top				;[0x000-0x003]
 	DCD		Start_Init				;[0x004-0x007]
-	SPACE	0x78			
-	DCD		DMA1_Stream5_IRQHandler	;[80]
-	DCD		DMA1_Stream6_IRQHandler	;[0x84]
-	SPACE	0x50
-	DCD		USART2_IRQHandler		;[0x0D8-0x0DC]
-	SPACE	0x44
+	SPACE	0x68			
+	DCD		DMA1_Stream1_IRQHandler	;[70]
+	SPACE	0x10
+	DCD		DMA1_Stream6_IRQHandler	;[0x80]	
+	SPACE	0x98
 	DCD		DMA2_Stream0_IRQHandler
 	
 	
@@ -22,36 +21,60 @@ Stack_Top
 	ENTRY
 	
 E0
+	
 	B	E0
 
 Start_Init
-
-;Uses USART1: TX = PB6, RX = PB7 (AF7)
+;PD9 - RX USART3
+;Uses USART2: TX = PD5
 	LDR			R0,			=RCC_BASE
-;USART1 CLK Enable
+;USART2 CLK Enable
 	LDR			R1,			[R0,		#RCC_APB1ENR]
 	ORR			R1,			R1,			#RCC_APB1ENR_USART2EN
 	STR			R1,			[R0,		#RCC_APB1ENR]
-;GPIOA CLK Enable
+;GPIOD CLK Enable
 	LDR			R1,			[R0,		#RCC_AHB1ENR]
 	ORR			R1,			R1,			#RCC_AHB1ENR_GPIODEN
 	STR			R1,			[R0,		#RCC_AHB1ENR]
-;PB6  = USART1_TX (AF7) | PB7 = USART1_RX (AF7)
+;PD5 = USART1_TX (AF7)
 	LDR			R0,			=GPIOD_BASE
 	LDR			R1,			[R0,		#GPIO_MODER]
 	BFC			R1,			#GPIO_MODER_MODER5_Pos,		#4
-	ORR			R1,			R1,			#(GPIO_MODER_MODER5_1 + GPIO_MODER_MODER6_1)
+	ORR			R1,			R1,			#GPIO_MODER_MODER5_1
 	STR			R1,			[R0,		#GPIO_MODER]
-;PB6 Pull Up | PB7 Pull Up
+;PD6 Pull Up
 	LDR			R1,			[R0,		#GPIO_PUPDR]
-	BFC			R1,			#GPIO_PUPDR_PUPD6_Pos,		#4
-	ORR			R1,			R1,			#(GPIO_PUPDR_PUPD5_0 + GPIO_PUPDR_PUPD6_0)
+	BFC			R1,			#GPIO_PUPDR_PUPD5_Pos,		#4
+	ORR			R1,			R1,			#GPIO_PUPDR_PUPD5_0
 	STR			R1,			[R0,		#GPIO_PUPDR]
 	LDR			R1,			[R0,		#GPIO_AFRL]
-	BFC			R1,			#GPIO_AFRL_AFSEL6_Pos,		#8
-	ORR			R1,			R1,			#(7 << GPIO_AFRL_AFSEL6_Pos)
+	BFC			R1,			#GPIO_AFRL_AFSEL5_Pos,		#8
 	ORR			R1,			R1,			#(7 << GPIO_AFRL_AFSEL5_Pos)
 	STR			R1,			[R0,		#GPIO_AFRL]
+;USART3 CLK Enable
+	LDR			R0,			=RCC_BASE
+	LDR			R1,			[R0,		#RCC_APB1ENR]
+	ORR			R1,			R1,			#RCC_APB1ENR_USART3EN
+	STR			R1,			[R0,		#RCC_APB1ENR]
+;GPIOD CLK Enable
+	LDR			R1,			[R0,		#RCC_AHB1ENR]
+	ORR			R1,			R1,			#RCC_AHB1ENR_GPIODEN
+	STR			R1,			[R0,		#RCC_AHB1ENR]
+;PD8  = USART1_TX (AF7)
+	LDR			R0,			=GPIOD_BASE
+	LDR			R1,			[R0,		#GPIO_MODER]
+	BFC			R1,			#GPIO_MODER_MODER9_Pos,		#4
+	ORR			R1,			R1,			#GPIO_MODER_MODER9_1 
+	STR			R1,			[R0,		#GPIO_MODER]
+;PD8 Pull Up
+	LDR			R1,			[R0,		#GPIO_PUPDR]
+	BFC			R1,			#GPIO_PUPDR_PUPD9_Pos,		#4
+	ORR			R1,			R1,			#GPIO_PUPDR_PUPD9_0
+	STR			R1,			[R0,		#GPIO_PUPDR]
+	LDR			R1,			[R0,		#GPIO_AFRH]
+	BFC			R1,			#GPIO_AFRH_AFSEL9_Pos,		#8
+	ORR			R1,			R1,			#(7 << GPIO_AFRH_AFSEL9_Pos)
+	STR			R1,			[R0,		#GPIO_AFRH]
 	
 ;USART1 BR = 57 600, 2 stop bits, no parity
 	LDR			R0,			=USART2_BASE
@@ -63,60 +86,70 @@ Start_Init
 	AND			R1,			R1,			#~USART_CR2_STOP
 	ORR			R1,			R1,			#USART_CR2_STOP_1
 	STR			R1,			[R0,		#USART_CR2]
+	LDR			R1,			[R0,		#USART_CR3]
+;	ORR			R1,			R1,			#USART_CR3_DMAR
+	ORR			R1,			R1,			#USART_CR3_DMAT
+	STR			R1,			[R0,		#USART_CR3]
 	LDR			R1,			[R0,		#USART_CR1]
 	ORR			R1,			R1,			#(USART_CR1_RE + USART_CR1_TE)
 	ORR			R1,			R1,			#(USART_CR1_RXNEIE + USART_CR1_TCIE)
 	ORR			R1,			R1,			#USART_CR1_UE
 	STR			R1,			[R0,		#USART_CR1]
-;USART1 Interrupts Enable
-	LDR			R0,			=NVIC_BASE
-	LDR			R1,			[R0,		#NVIC_ISER1]
-	ORR			R1,			R1,			#(1 << (USART2_IRQn - 32))
-	STR			R1,			[R0,		#NVIC_ISER1]
+;USART3 CONFIG	
+	LDR			R0,			=USART3_BASE
+;F_APB2 = 16 MHz
+;BRR = (16 MHz -  (57 600 / 2)) / 57 600 = 277
+	MOV			R1,			#277
+	STR			R1,			[R0,		#USART_BRR]
+	LDR			R1,			[R0,		#USART_CR2]
+	AND			R1,			R1,			#~USART_CR2_STOP
+	ORR			R1,			R1,			#USART_CR2_STOP_1
+	STR			R1,			[R0,		#USART_CR2]
+	LDR			R1,			[R0,		#USART_CR3]
+	ORR			R1,			R1,			#USART_CR3_DMAR
+;	ORR			R1,			R1,			#USART_CR3_DMAT
+	STR			R1,			[R0,		#USART_CR3]
+	LDR			R1,			[R0,		#USART_CR1]
+	ORR			R1,			R1,			#(USART_CR1_RE + USART_CR1_TE)
+	ORR			R1,			R1,			#(USART_CR1_RXNEIE + USART_CR1_TCIE)
+	ORR			R1,			R1,			#USART_CR1_UE
+	STR			R1,			[R0,		#USART_CR1]
 
 
-;DMA1 Mem to Mem Init
+;DMA1 Per to Mem Init
 	LDR			R0,			=RCC_BASE
 	LDR			R1,			[R0,		#RCC_AHB1ENR]
-	ORR			R1,			R1,			#RCC_AHB1ENR_DMA2EN
+	ORR			R1,			R1,			#RCC_AHB1ENR_DMA1EN
 	STR			R1,			[R0,		#RCC_AHB1ENR]
 ;1
 	LDR			R0,			=DMA1_BASE
-	LDR			R1,			[R0,		#DMA_S0CR]
+	LDR			R1,			[R0,		#DMA_S1CR]
 	AND			R1,			R1,			#DMA_SxCR_EN
 	CMP			R1,			#0
 	BNE			E0
 ;2
-	LDR			R1,			=(USART2_BASE + USART_DR)
-	STR			R1,			[R0,	#DMA_S0PAR]
+	LDR			R1,			=(USART3_BASE + USART_DR)
+	STR			R1,			[R0,	#DMA_S1PAR]
 ;3
 	LDR			R1,			=(SRAM1_BASE + Array_1)
-	STR			R1,			[R0,	#DMA_S0M0AR]
+	STR			R1,			[R0,	#DMA_S1M0AR]
 ;4
-
+	MOV			R1,			#10
+	STR			R1,			[R0,	#DMA_S1NDTR]
 ;Skip 5, 6, 7 and 8
 ;9
-	LDR			R1,			[R0,	#DMA_S5CR]
-	;LDR	R1,	[ DMA_S5CR_HSEL]
+	LDR			R1,			[R0,	#DMA_S1CR]
 	AND			R1,			R1,		#~DMA_SxCR_CHSEL
 	ORR			R1,			#(4 << DMA_SxCR_CHSEL_Pos)
 	AND			R1,			R1,		#~DMA_SxCR_DIR		
-	ORR			R1,			R1,		#(DMA_SxCR_MINC + DMA_SxCR_PINC)
+	ORR			R1,			R1,		#DMA_SxCR_MINC
 	ORR			R1,			R1,		#DMA_SxCR_TCIE
-	STR			R1,			[R0,	#DMA_S5CR]
+	STR			R1,			[R0,	#DMA_S1CR]
 ;DMA1_S0 Interrupt Enable
 	LDR			R2,			=NVIC_BASE
-	LDR			R1,			[R2,	#NVIC_ISER1]
-	ORR			R1,			R1,	#(1 << (DMA1_Stream5_IRQn - 32))
-	STR			R1,			[R2,	#NVIC_ISER1]
-
-	
-	
-
-;DMA2 S0 Enable
-;	LDR			R1,			[R0,	#DMA_S0CR]
-;	ORR			R1,			R1,		#DMA_SxCR_EN
-;	STR			R1,			[R0,	#DMA_S0CR]
+	LDR			R1,			[R2,	#NVIC_ISER0]
+	ORR			R1,			R1,		#(1 << DMA1_Stream1_IRQn)
+	STR			R1,			[R2,	#NVIC_ISER0]
 
 ;DMA2 Mem to Mem Init
 	LDR			R0,			=RCC_BASE
@@ -128,6 +161,7 @@ Start_Init
 	LDR			R1,			[R0,	#DMA_S0CR]
 	AND			R1,			R1,		#DMA_SxCR_EN
 	CMP			R1,			#0
+	STR			R1,			[R0,	#DMA_S0CR]
 	BNE	E0
 ;2
 	LDR			R1,			=(SRAM1_BASE + Array_1)
@@ -151,67 +185,93 @@ Start_Init
 	LDR			R1,			[R2,	#NVIC_ISER1]
 	ORR			R1,			R1,		#(1 << (DMA2_Stream0_IRQn - 32))
 	STR			R1,			[R2,	#NVIC_ISER1]
-;Array_1 Init
 	
-;DMA2 S0 Enable
-	LDR			R1,			[R0,	#DMA_S0CR]
-;	ORR			R1,			R1,		#DMA_SxCR_EN
-	STR			R1,			[R0,	#DMA_S0CR]
-;	\|/
+;DMA1 S6 ENABLE
+;DMA1 MEM to PER Init
+	LDR			R0,			=RCC_BASE
+	LDR			R1,			[R0,		#RCC_AHB1ENR]
+	ORR			R1,			R1,			#RCC_AHB1ENR_DMA1EN
+	STR			R1,			[R0,		#RCC_AHB1ENR]
+;1
+	LDR			R0,			=DMA1_BASE
+	LDR			R1,			[R0,		#DMA_S6CR]
+	AND			R1,			R1,			#DMA_SxCR_EN
+	CMP			R1,			#0
+	BNE			E0
+;2
+	LDR			R1,			=(USART2_BASE + USART_DR)
+	STR			R1,			[R0,	#DMA_S6PAR]
+;3
+	LDR			R1,			=(SRAM1_BASE + Array_2)
+	STR			R1,			[R0,	#DMA_S6M0AR]
+;4
+	MOV			R1,			#10
+	STR			R1,			[R0,	#DMA_S6NDTR]
+;Skip 5, 6, 7 and 8
+;9
+	LDR			R1,			[R0,	#DMA_S6CR]
+	AND			R1,			R1,		#~DMA_SxCR_CHSEL
+	ORR			R1,			#(4 << DMA_SxCR_CHSEL_Pos)
+	AND			R1,			R1,		#~DMA_SxCR_DIR
+	ORR			R1,			R1,		#DMA_SxCR_DIR_0	
+	ORR			R1,			R1,		#DMA_SxCR_MINC
+	ORR			R1,			R1,		#DMA_SxCR_TCIE
+	STR			R1,			[R0,	#DMA_S6CR]
+;DMA1_S0 Interrupt Enable
+	LDR			R2,			=NVIC_BASE
+	LDR			R1,			[R2,	#NVIC_ISER0]
+	ORR			R1,			R1,		#(1 << DMA1_Stream6_IRQn)
+	STR			R1,			[R2,	#NVIC_ISER0]
+	
+	
+	
+	LDR			R0,			=DMA1_BASE
+	LDR			R1,			[R0,	#DMA_S1CR]
+	ORR			R1,			R1,		#DMA_SxCR_EN
+	STR			R1,			[R0,	#DMA_S1CR]	
+	
+
 Main_Loop
 	B	Main_Loop
+			
+DMA1_Stream1_IRQHandler
+	LDR			R0,			=DMA1_BASE
+	LDR			R1,			[R0,	#DMA_LIFCR]
+	ORR			R1,			R1,		#DMA_LIFCR_CTCIF1
+	ORR			R1,			R1,		#DMA_LIFCR_CHTIF1
+	STR			R1,			[R0,	#DMA_LIFCR]
+;DMA2 S0 Enable
+	LDR			R0,			=DMA2_BASE
+	LDR			R1,			[R0,	#DMA_S0CR]
+	ORR			R1,			R1,		#DMA_SxCR_EN
+	STR			R1,			[R0,	#DMA_S0CR]
+	BX			LR
 	
 DMA2_Stream0_IRQHandler
 	LDR			R0,			=DMA2_BASE
 	LDR			R1,			[R0,	#DMA_LIFCR]
 	ORR			R1,			R1,		#(DMA_LIFCR_CTCIF0 + DMA_LIFCR_CHTIF0)
 	STR			R1,			[R0,	#DMA_LIFCR]
+;DMA2 S6 Enable
+	LDR			R0,			=DMA1_BASE
+	LDR			R1,			[R0,	#DMA_S6CR]
+	ORR			R1,			R1,		#DMA_SxCR_EN
+	STR			R1,			[R0,	#DMA_S6CR]
 	BX			LR
-		
-	
-	
-DMA1_Stream5_IRQHandler
-	MOV			R1,			#10
-	STR			R1,			[R0,	#DMA_S0NDTR]
-	BX		LR
 	
 DMA1_Stream6_IRQHandler
-	BX		LR
-
-USART2_IRQHandler
-	LDR			R0,			=USART2_BASE
-	LDR			R1,			[R0,		#USART_SR]
-;Check RXNE
-	AND			R2,			R1,			#USART_SR_RXNE
-	CMP			R2,			#USART_SR_RXNE
-	BEQ			USART2_IRQHandler_RXNE
-;Check TC
-	AND			R2,			R1,			#USART_SR_TC
-	CMP			R2,			#USART_SR_TC
-	BEQ			USART2_IRQHandler_TC
-;Check ORE
-	AND			R2,			R1,			#USART_SR_ORE
-	CMP			R2,			#USART_SR_ORE
-	BEQ			USART2_IRQHandler_ORE
-	BX			LR
-
-USART2_IRQHandler_TC
-	AND			R1,			R1,			#~USART_SR_TC
-	STR			R1,			[R0,		#USART_SR]
-	BX			LR
-
-USART2_IRQHandler_RXNE
-	LDR			R1,			[R0,		#USART_DR]
-	STR			R1,			[R0,		#USART_DR]
-	
-;DMA1 S0 Enable
 	LDR			R0,			=DMA1_BASE
-	LDR			R1,			[R0,	#DMA_S0CR]
+	LDR			R1,			[R0,	#DMA_HIFCR]
+	ORR			R1,			R1,		#DMA_HIFCR_CTCIF6
+	ORR			R1,			R1,		#DMA_HIFCR_CHTIF6
+	STR			R1,			[R0,	#DMA_HIFCR]
+;DMA1 S5 Enable
+	LDR			R0,			=DMA1_BASE
+	LDR			R1,			[R0,	#DMA_S1CR]
 	ORR			R1,			R1,		#DMA_SxCR_EN
-	STR			R1,			[R0,	#DMA_S0CR]
+	STR			R1,			[R0,	#DMA_S1CR]
 	BX			LR
 
-USART2_IRQHandler_ORE
-	BX			LR
-	
+
+
 	END
